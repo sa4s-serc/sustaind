93b6489f465b3a93ae19d822b651e6249734d854
Arihant Tripathy <arihanttr@live.com>
2025-08-02 03:57:14 +0000

    Try #4

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: true,
-  // basePath: "/sustaind",
-  // assetPrefix: "/sustaind/",
+  // trailingSlash: true,
+  basePath: isProd ? '/sustaind' : '',
+  assetPrefix: isProd ? '/sustaind/' : '',
   images: {
     unoptimized: true,
   },
