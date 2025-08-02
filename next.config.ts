5f2fbc7a7b635757d86bcc70cdbc815d96f83961
Arihant Tripathy <arihanttr@live.com>
2025-08-02 03:50:50 +0000

    Try #2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  assetPrefix: "/sustaind",
+  basePath: "/sustaind",
+  assetPrefix: "/sustaind/",
   images: {
     unoptimized: true,
   },
