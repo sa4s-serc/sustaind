ad6720dcdf73acac080fb95f9415abee5605566c
Arihant Tripathy <arihanttr@live.com>
2025-08-03 05:32:58 +0000

    Fix assetPrefix and basePath for production environment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
+
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: "export",
   distDir: 'docs',
-  assetPrefix: isProd ? '/sustaind/docs' : '',
+  assetPrefix: isProd ? '/sustaind' : '',
+  basePath: isProd ? '/sustaind' : '',
   images: {
     unoptimized: true,
   },
